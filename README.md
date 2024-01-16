523aca1eb9386f4ccafe627a6dbcc12993914c8d
Kholis_7 <kholissetia17@gmail.com>
2024-01-17 00:03:59 +0700

    Penambahan Index Kasir
    Menambah Index Kasir

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # POS_UKK
+
+Aplikasi berbasis website Aplikasi Kasir atau biasa disebut POS (Point Of Sale) Pada ujikom (Uji Kompetensi) Program keahlian Rekayasa Perangkat Lunak tahun 2024
---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Apliasi Kasir || UJIKOM RPL</title>
+</head>
+
+<body>
+  <a href="kasir/index.php">Login</a>
+</body>
+
+</html>
